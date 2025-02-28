bf562177842fb9e66ddcf3e51155dcf7ca1e0285
Proff Milo <emileesapa@gmail.com>
2025-02-28 05:26:44 -0600

    code updated with cypress hooks

--- cypress/e2e/saucedemo/login.cy.js
+++ cypress/e2e/saucedemo/login.cy.js
@@ -1,13 +1,22 @@
 /// <reference types="cypress" />
 
+let standardUser='standard_user'
+let problemUser="problem_user"
+let lockedOutUser="locked_out_user"
+let password='secret_sauce'
+
+
 describe('Saucedemo - Login Test',()=>{
 
 
-    it('Error Messages - Login page', ()=>{
-
+    beforeEach(() => {
         cy.visit('https://www.saucedemo.com/')//navigate to url
         cy.title().should('eq','Swag Labs')
-        cy.get('.login_logo').should('eq',)
+        cy.get('.login_logo').should('contain','Swag Labs')
+      })
+
+    it('Error Messages - Login page', ()=>{
+
         cy.get('[data-test="login-button"]').click()
         cy.get('h3').should('contain.text','Epic sadface: Username is required')
         cy.url().should('eq','https://www.saucedemo.com/')//assert
@@ -16,11 +25,8 @@ describe('Saucedemo - Login Test',()=>{
 
 
 it('Standard user - Login page', ()=>{
-
-    cy.visit('https://www.saucedemo.com/')//navigate to url
-    cy.title().should('eq','Swag Labs')//assert
-    cy.get('#user-name').type('standard_user')
-    cy.get('#password').type('secret_sauce')
+    cy.get('#user-name').type(standardUser)
+    cy.get('#password').type(password)
     cy.get('[data-test="login-button"]').click()
     cy.url().should('contain','/inventory.html')//assert
     cy.get('#react-burger-menu-btn').click()
@@ -31,16 +37,23 @@ it('Standard user - Login page', ()=>{
 
 it('Locked out user - Login page', ()=>{
 
-    cy.visit('https://www.saucedemo.com/')//navigate to url
-    cy.title().should('eq','Swag Labs')//assert
-    cy.get('#user-name').type('locked_out_user')
-    cy.get('#password').type('secret_sauce')
+    cy.get('#user-name').type(lockedOutUser)
+    cy.get('#password').type(password)
     cy.get('.submit-button').click()
     cy.get('[data-test="error"]').should('contain','Epic sadface: Sorry, this user has been locked out.')//assert
     cy.url().should('eq','https://www.saucedemo.com/')//assert
 
 })
 
+it('Problem user - Login page', ()=>{
+    cy.get('#user-name').type(problemUser)
+    cy.get('#password').type(password)
+    cy.get('.submit-button').click()
+    cy.get('[src="/static/media/sauce-backpack-1200x1500.0a0b85a3.jpg"]')
+        .should('be.visible')//assert that the back pack is present
+   
+
+})
 
 
 })
